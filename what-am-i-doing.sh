e37091b9b2c253e31efaa053a80b555a687d08e0
cyslas <cyslas@switchfly.com>
2014-04-03 18:23:16 +0000

    Add Mac OS support

--- what-am-i-doing.sh
+++ what-am-i-doing.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+
 minutes_between_pictures=10
+kernel=$(uname -a)
+
+if [[ $kernel =~ .*Darwin.* ]] # Check if Mac OS
+then
+    capture_command() {
+	imagesnap -w 1.5 00000001.png
+    }
+else 
+    capture_command() {
+	mplayer -vo png -frames 1 tv:// -quiet
+    }
+fi
 
 sleep_interval="$((60*$minutes_between_pictures))"
 while [ 1 -le 2 ]
 do
     directory_name=$(date +"%F")
     mkdir -p $directory_name
-    mplayer -vo png -frames 1 tv:// -quiet
+    capture_command
     filename=$(date +"%T").png
     mv 00000001.png $directory_name/$filename
     sleep $sleep_interval
